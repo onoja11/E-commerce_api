39775a0ac140a6ed27926359962c803efcb27589
Veritas <ochigbogodswill868@gmail.com>
2025-08-04 15:00:01 -0700

    implemented authentication in authcontroller

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/',function(){
+    return response()->json(['message' => 'Welcome to the API']);
+});
+Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
+Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->middleware('auth:sanctum');
