02ad27cd36635b0c9375f0c248344c8497b43cbb
Veritas <ochigbogodswill868@gmail.com>
2025-09-03 16:29:21 -0700

    updated cors and dockerfile

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://e-commerce-prll.onrender.com/'],
+    'allowed_origins' => ['https://e-commerce-prll.onrender.com'],
 
     'allowed_origins_patterns' => [],
 
