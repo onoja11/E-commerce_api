50d909fcab64b8ddbfb93065ebcf668fdc9f9214
Veritas <ochigbogodswill868@gmail.com>
2025-09-08 14:28:42 -0700

    updated wallet

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://e-commerce-prll.onrender.com'],
+    'allowed_origins' => ['https://e-commerce-prll.onrender.com','http://localhost:3000/'],
 
     'allowed_origins_patterns' => [],
 
