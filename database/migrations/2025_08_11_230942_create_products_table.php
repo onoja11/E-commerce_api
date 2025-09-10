fd53c63eb79637ce6b8227fe5397c5c9ec1eceeb
Veritas <ochigbogodswill868@gmail.com>
2025-09-10 14:25:55 -0700

    updated migrations

--- database/migrations/2025_08_11_230942_create_products_table.php
+++ database/migrations/2025_08_11_230942_create_products_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->decimal('price',10,2);
             $table->integer('stock')->default(0);
             $table->string('image')->nullable();
-            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
             $table->timestamps();
         });
     }
